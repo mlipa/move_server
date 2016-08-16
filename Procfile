b1328ef628215beac80890efee7edd6ea582e7f7
Marcin Lipa <marcin.lipa@protonmail.com>
2016-08-16 16:02:42 +0200

    Prepare for Heroku deploy

---
+++ Procfile
@@ -0,0 +1,5 @@
+web: gunicorn application:application --log-file=-
+create: python scripts/database_create.py
+downgrade: python scripts/database_downgrade.py
+migrate: python scripts/database_migrate.py
+upgrade: python scripts/database_upgrade.py
--- configuration.py
+++ configuration.py
@@ -10,7 +10,8 @@ class Configuration(object):
     DEBUG = True
     HASHING_METHOD = 'sha256'
     HASHING_ROUND = 1
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(PROJECT_DIRECTORY, 'move.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(PROJECT_DIRECTORY, 'move.db')
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
     SQLALCHEMY_MIGRATE_REPO = os.path.join(PROJECT_DIRECTORY, 'database')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOADED_AVATARS_DEST = os.path.join(PROJECT_DIRECTORY, 'application/static/images/avatars')

a1c239d2ab172776af0e6683e244e47ef303ff2a
Marcin Lipa <marcin.lipa@protonmail.com>
2016-08-31 18:52:39 +0200

    Add activities and predictions table

--- application/models.py
+++ application/models.py
@@ -10,9 +10,25 @@ from application import database
 APPLICATION_DIRECTORY = os.path.abspath(os.path.dirname(__file__))
 
 
+class Activities(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    name = database.Column(database.String(32), index=True, unique=True)
+    predictions = database.relationship('Predictions', backref='activity', lazy='dynamic')
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Activity %r>' % self.name
+
+
 class Classifiers(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(32), index=True, unique=True)
+    predictions = database.relationship('Predictions', backref='classifier', lazy='dynamic')
     users = database.relationship('Users', backref='classifier', lazy='dynamic')
 
     def get_id(self):
@@ -25,6 +41,26 @@ class Classifiers(database.Model):
         return '<Classifier %r>' % self.name
 
 
+class Predictions(database.Model):
+    id = database.Column(database.Integer, primary_key=True)
+    timestamp = database.Column(database.DateTime, index=True, unique=False)
+    activity_id = database.Column(database.Integer, database.ForeignKey('activities.id'))
+    classifier_id = database.Column(database.Integer, database.ForeignKey('classifiers.id'))
+    user_id = database.Column(database.Integer, database.ForeignKey('users.id'))
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __init__(self, timestamp, activity_id, classifier_id, user_id):
+        self.timestamp = timestamp
+        self.activity_id = activity_id
+        self.classifier_id = classifier_id
+        self.user_id = user_id
+
+    def __repr__(self):
+        return '<Prediction %r>' % self.activity.name
+
+
 class Users(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     name = database.Column(database.String(64), index=True, unique=False)
@@ -33,6 +69,7 @@ class Users(database.Model):
     password = database.Column(database.String(64), index=False, unique=False)
     salt = database.Column(database.String(8), index=False, unique=False)
     classifier_id = database.Column(database.Integer, database.ForeignKey('classifiers.id'))
+    predictions = database.relationship('Predictions', backref='user', lazy='dynamic')
 
     @staticmethod
     def is_active():
